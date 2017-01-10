32bbba5a7be2f17ffa70cace41d3c97e2b6080bd
gayanj <gayan@bluedrop.com>
2017-01-10 09:46:20 -0330

    Initial Commit

---
+++ weeklyweather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weeklyweather
+//
+//  Created by Gayan Jayasundara on 2017-01-10.
+//  Copyright © 2017 Gayan Jayasundara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
